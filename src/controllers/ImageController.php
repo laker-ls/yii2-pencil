a8509b742e3bee8c77b83472362695ad557c7729
laker-ls <clear.live@bk.ru>
2019-10-02 15:07:20 +0300

    Исправлена ошибка возникающая, при удалении изображения

--- src/controllers/ImageController.php
+++ src/controllers/ImageController.php
@@ -103,10 +103,7 @@ class ImageController extends Controller
      */
     public function actionDelete()
     {
-        $this->enableCsrfValidation = false;
         $post = Yii::$app->request->post();
-
-        echo '<pre>'; print_r(1); echo '</pre>'; die;
 
         $id = $post['id'];
         $id = mb_substr(mb_strstr($id, '-'), 1, mb_strlen($id)); // удаление лишних данных из переданной строки.
